e453d0b7c502685ff6ee7a1ae04f80fc8ad193aa
TLSLime <15953210290@163.com>
2024-10-04 15:24:34 +0800

    Site updated: 2024-10-04 15:24:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["post/5a717bcc.html","post/6a717bf0.html","post/5b17b176.html","post/4a17b156.html","post/5a717bf0.html"];function toRandomPost(){
+var posts=["post/5b17b176.html","post/4a17b156.html","post/5a717bf0.html","post/6a717bf0.html","post/5a717bcc.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
