dc886c262ee84bc4140eede268c28675c0d632d9
TLSLime <15953210290@163.com>
2024-10-04 13:51:47 +0800

    Site updated: 2024-10-04 13:51:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/4a17b156.html","post/4a17b156.html","post/5b17b176.html","post/5a717bf0.html","post/6a717bf0.html","post/5a717bcc.html"];function toRandomPost(){
+var posts=["post/5b17b176.html","post/4a17b156.html","post/5a717bf0.html","post/5a717bcc.html","post/6a717bf0.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
