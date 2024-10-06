fe16c78e5dcc9559aaa340f5b5687ff9e8c6b392
TLSLime <15953210290@163.com>
2024-10-06 14:51:10 +0800

    Site updated: 2024-10-06 14:51:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["post/5a717bcc.html","post/4a17b156.html","post/5b17b176.html","post/4d1df81a.html","post/5a717bf0.html","post/5c17b176.html","post/6a717bf0.html","post/b687ad23.html"];function toRandomPost(){
+var posts=["post/5b17b176.html","post/4a17b156.html","post/4d1df81a.html","post/5c17b176.html","post/5a717bf0.html","post/5a717bcc.html","post/b687ad23.html","post/6a717bf0.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
